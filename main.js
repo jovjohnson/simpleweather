c1417545e2872b8b45d224dc2828025ed47b3695
Jovvi Johnson <jjohnsow@gmail.com>
2016-06-24 14:50:45 -0700

    google maps

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(document).ready(init);
 
 function init() {
 
+  initMap();
+
   var name;
   var temperature;
   var humidity;
@@ -75,6 +77,15 @@ function init() {
     //   $icon.addClass('wi-day-sunny');
     // }
   }
+
+  function initMap() {
+  // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        scrollwheel: false,
+        zoom: 8
+      });
+}
 
 
 }
